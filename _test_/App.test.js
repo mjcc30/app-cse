794cb2d6d64d070bae5875d21d21a3dea5f14f31
mjcc30 <maxime.cordeiro@gmail.com>
2020-08-13 21:58:27 +0200

    test platform

--- _test_/App.test.js
+++ _test_/App.test.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
 
-import App from '../App';
+jest.useFakeTimers();
 
-describe('<App />', () => {
-  it('has 1 child', () => {
-    const tree = renderer.create(<App />).toJSON();
-    expect(tree.children.length).toBe(1);
-  });
+import { Platform } from 'react-native';
+
+it(`Platform.OS works as expected`, () => {
+  expect(Platform.OS).toMatchSnapshot();
 });
