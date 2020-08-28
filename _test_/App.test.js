0b96ef7783ef092a81504d1d9c6270367e85eac2
mjcc30 <maxime.cordeiro@gmail.com>
2020-08-28 19:59:43 +0200

    update

--- _test_/App.test.js
+++ _test_/App.test.js
@@ -2,7 +2,13 @@ import React from 'react';
 import { Platform } from 'react-native';
 import renderer from 'react-test-renderer';
 
+import App from '../App';
+
 jest.useFakeTimers();
+
+it('renders correctly', () => {
+  renderer.create(<App />);
+});
 
 it(`Platform.OS works as expected`, () => {
   expect(Platform.OS).toMatchSnapshot();
