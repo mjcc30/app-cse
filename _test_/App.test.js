401f2e71799633cf4b9f18dd0c1dcb71f3bbe581
mjcc30 <maxime.cordeiro@gmail.com>
2020-08-28 19:31:41 +0200

    update

--- _test_/App.test.js
+++ _test_/App.test.js
@@ -2,8 +2,6 @@ import React from 'react';
 import { Platform } from 'react-native';
 import renderer from 'react-test-renderer';
 
-import App from '../App';
-
 jest.useFakeTimers();
 
 it(`Platform.OS works as expected`, () => {
